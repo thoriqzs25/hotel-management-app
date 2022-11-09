1ab6af86a8343e2dc85cd0ba23fe4337dd9c48d4
mrsibatuaras <sibatuarachristian@gmail.com>
2022-11-10 02:47:23 +0700

    FnbInformation: add image and button

--- src/content/FnBInformation/index.js
+++ src/content/FnBInformation/index.js
@@ -3,25 +3,29 @@ const DATA = [
       title: 'Ayam Goreng',
       availability: true,
       price: 12000,
-      discount: 10000,
+      discount: 0,
+      image: "../../public/images/1.jpg",
     },
     {
       title: 'Rendang Sapi',
       availability: true,
       price: 12000,
       discount: 10000,
+      image: "../../public/images/2.jpg",
     },
     {
       title: 'Nasi Goreng',
       availability: true,
-      price: 12000,
+      price: 20000,
       discount: 10000,
+      image: "../../public/images/3.jpg",
     },
     {
       title: 'Magelangan Rendang',
       availability: false,
       price: 12000,
       discount: 0,
+      image: "../../public/images/4.jpg",
     },
     
   ];
@@ -37,27 +41,35 @@ export function generateFnBInformation() {
             availability = "Not Available"
         }
 
-        let discount = ""
+        let discount = "&nbsp"
         let lineThrough = ""
         if (res.discount > 0) {
-            discount = res.discount
+            discount = "Rp " + res.discount
             lineThrough = "line-through"
         }
+
+        let btnstatus = ""
+        if (res.availability == false) {
+            btnstatus = "disabled"
+        } 
 
         const field = `
             <div class="card" id="fnb-information">
                 <div class="fnb-name">
                     <p>${res.title}</p>
                 </div>
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="images/office.jpg">
+                <div class="card-image" id="fnb-image">
+                    <img src="${res.image}">
                 </div>
                 <div class="card-content">
                     <div class="price-container">
-                        <p class="${lineThrough}">${res.price}</p>
+                        <p class="${lineThrough}">Rp ${res.price}</p>
                         <p>${discount}</p>
                     </div>
-                    <p class="${availability}">${availability}</p>
+                </div>
+                <div class="card-bottom">
+                        <p class="${availability}">${availability}</p>
+                        <a class="btn modal-trigger button purple normal-text ${btnstatus}" id="add-button" href="#">Add</a>
                 </div>
             </div>
         `;
